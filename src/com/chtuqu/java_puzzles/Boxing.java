9a03d63c65eda29e18fa240476e8a771d0d9acdb
Michael Smith <ketrimf@gmail.com>
2017-01-20 08:38:57 +0100

    Added example on boxing

---
+++ src/com/chtuqu/java_puzzles/Boxing.java
@@ -0,0 +1,13 @@
+package com.chtuqu.java_puzzles;
+
+public class Boxing {
+    static Integer x;
+    public static void main(String[] args) {
+        doStuff(x);     // exception on this line
+    }
+
+    static void doStuff(int z) {
+        int z2 = 5;
+        System.out.println(z2 + z); // not on this line
+    }
+}
