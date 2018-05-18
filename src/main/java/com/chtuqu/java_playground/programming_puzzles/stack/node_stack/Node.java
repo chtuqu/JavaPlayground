a7e9ba291d2258e728c73be2b1bb309eb30d5eed
Michael Smith <ketrimf@gmail.com>
2018-05-19 00:13:08 +0300

    Move algorithmic examples to a different project

--- src/main/java/com/chtuqu/java_playground/programming_puzzles/stack/node_stack/Node.java
+++
@@ -1,9 +0,0 @@
-package com.chtuqu.java_playground.programming_puzzles.stack.node_stack;
-
-public interface Node<T> {
-    T getValue();
-    void setValue(T t);
-
-    Node<T> getNext();
-    void setNext(Node<T> t);
-}
